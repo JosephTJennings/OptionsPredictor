47a626570c015fe09133d50ef02474a523dead7b
JosephTJennings <josephjenningsct@gmail.com>
2023-04-19 09:16:06 -0500

    Condensed all functions into one main function.

--- KNNOptions.py
+++ KNNOptions.py
@@ -6,8 +6,8 @@ from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import f1_score
 
-def predictCalls():
-    seeds = pd.read_csv('AAPL_calls.csv', sep = ',')
+def predictKNN(filename):
+    seeds = pd.read_csv(filename, sep = ',')
     data = seeds.iloc[:, [0, 5, 6, 7, 8, 9, 10]] #Select optionsExpire,strikePrice,volume,delta,gamma,iv,rho,theta,vega
                                                 #           0                3       4      5     6    7  8    9    10
                                                 
@@ -22,60 +22,17 @@ def predictCalls():
     accuracy = accuracy_score(y_test, y_pred)
     precision = precision_score(y_test, y_pred)
     recall = recall_score(y_test, y_pred)
-    print("-------CALLS PREDICITON-------")
-    print("Accuracy:", accuracy)
-    print("Precision:", precision)
-    print("Recall:", recall)
-    print("")
-    
-def predictPuts():
-    seeds = pd.read_csv('AAPL_puts.csv', sep = ',')
-    data = seeds.iloc[:, [0, 5, 6, 7, 8, 9, 10]] #Select optionsExpire,strikePrice,volume,delta,gamma,iv,rho,theta,vega
-                                                #           0                3       4      5     6    7  8    9    10
-                                                
-                                                #Can increase accuracy by only selecting: optionsExpire, delta, gamma, iv, rho, theta, gamma  (0, 5, 6, 7, 8, 9, 10)
-    labels = seeds.iloc[:, [1]] #Select inTheMoney
-    x_train, x_test, y_train, y_test = train_test_split(data,labels, test_size=0.2, random_state=1 )
-    knn = KNeighborsClassifier() #n_neighbors=30
-    knn.fit(x_train, y_train.values.ravel())
-
-    y_pred = knn.predict(x_test)
-
-    accuracy = accuracy_score(y_test, y_pred)
-    precision = precision_score(y_test, y_pred)
-    recall = recall_score(y_test, y_pred)
-    print("-------PUTS PREDICITON-------")
-    print("Accuracy:", accuracy)
-    print("Precision:", precision)
-    print("Recall:", recall)
-    print("")
-
-def predictMain():
-    seeds = pd.read_csv('AAPL_main.csv', sep = ',')
-    data = seeds.iloc[:, [0, 5, 6, 7, 8, 9, 10]] #Select optionsExpire,strikePrice,volume,delta,gamma,iv,rho,theta,vega
-                                                #           0                3       4      5     6    7  8    9    10
-                                                
-                                                #Can increase accuracy by only selecting: optionsExpire, delta, gamma, iv, rho, theta, gamma  (0, 5, 6, 7, 8, 9, 10)
-    labels = seeds.iloc[:, [1]] #Select inTheMoney
-    x_train, x_test, y_train, y_test = train_test_split(data,labels, test_size=0.2, random_state=1 )
-    knn = KNeighborsClassifier() #n_neighbors=30
-    knn.fit(x_train, y_train.values.ravel())
-
-    y_pred = knn.predict(x_test)
-
-    accuracy = accuracy_score(y_test, y_pred)
-    precision = precision_score(y_test, y_pred)
-    recall = recall_score(y_test, y_pred)
-    print("-------MAIN PREDICITON-------")
+    optionsType = filename.split("_")[1].split(".")[0]
+    print("-------" + optionsType + " PREDICITON-------")
     print("Accuracy:", accuracy)
     print("Precision:", precision)
     print("Recall:", recall)
     print("")
     
 def main():
-    predictCalls()
-    predictPuts()
-    predictMain()
+    predictKNN("AAPL_main.csv")
+    predictKNN("AAPL_puts.csv")
+    predictKNN("AAPL_calls.csv")
     
 if __name__ == "__main__":
     main()
