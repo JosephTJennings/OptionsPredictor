7e821a6a9506b34d0f0c88d33ef68f48bd81683a
Robby <riceslif@gmail.com>
2023-04-19 16:58:59 -0500

    Random Forest file

---
+++ RFOptions.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+
+
+def predictRF(filename):
+    df = pd.read_csv(filename)
+
+    # [0, 3, 4, 5, 6, 7, 8, 9, 10]
+    X = df.iloc[:, [0, 5, 6, 7, 8, 9, 10]]
+    y = df.iloc[:, [1]]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+
+    clf = RandomForestClassifier()
+    clf.fit(X_train, y_train)
+
+    y_pred = clf.predict(X_test)
+
+    print(f'!! Scores for {filename}:')
+    print(f'Accuracy: {accuracy_score(y_test, y_pred):.2f}')
+    print(f'Precision: {precision_score(y_test, y_pred):.2f}')
+    print(f'Recall: {recall_score(y_test, y_pred):.2f}\n')
+
+
+
+if __name__ == "__main__":
+    predictRF('./AAPL_main.csv')
+    predictRF('./AAPL_calls.csv')
+    predictRF('./AAPL_puts.csv')
